4eda0df2d0886b2aa6b1b4b6728bb00064ec2818
Osivwi Okiti <osivwiokiti@gmail.com>
2020-03-06 03:08:55 +0100

    Initial setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="home">
+  <div class="font-light py-2 text-xl uppercase tracking-wider bg-dark-gray">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+<style lang="scss" scoped>
 
+</style>
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
