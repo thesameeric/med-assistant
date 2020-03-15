12e8710e404737d23997db92fc523ce62badf984
okiti <osivwiokiti@gmail.com>
2020-03-15 01:47:12 +0100

    added how it work section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,12 @@
         </h1>
         <!-- eslint-disable-next-line max-len -->
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>Repellat suscipit laudantium quod nulla neque cum dolores cupiditate ducimus in eveniet harum minus placeat,</p>
+        <div class="flex mt-10 text-xl">
+          <a class="flex py-3 px-10 items-center bg-white hover:text-blue-600" href="">
+            <i class="text-3xl uil uil-android-alt"></i>
+            Download
+          </a>
+        </div>
       </div>
       <img class="ello ml-20 hidden lg:flex" src="../assets/img/S9.png" alt="">
     </section>
@@ -49,6 +55,39 @@
       </ul>
     </section>
     <!-- eslint-disable-next-line max-len -->
+    <section class="flex flex-col items-center flex-no-wrap p-5 lg:px-24 py-8 justify-between w-full">
+      <h1 class="font-bold mb-30 text-5xl">How it work</h1>
+      <div class="flex flex-row items-center justify-center">
+        <div class="flex flex-col w-5/12 bg-gray-200 p-10">
+          <span>
+            <!-- eslint-disable -->
+            <h3 class="font-semibold text-xl pb-3">Select symptoms or an illness</h3>
+            <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nihil animi nam facere eveniet ipsum doloribus a itaque, delectus maxime quidem corporis rem magni? Vero adipisci quae laboriosam asperiores facilis!</p>
+          </span>
+          <span>
+            <!-- eslint-disable -->
+            <h3 class="font-semibold text-xl pb-3">Get your diagnostics</h3>
+            <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nihil animi nam facere eveniet ipsum doloribus a itaque, delectus maxime quidem corporis rem magni? Vero adipisci quae laboriosam asperiores facilis!</p>
+          </span>
+          <span>
+            <!-- eslint-disable -->
+            <h3 class="font-semibold text-xl pb-3">Get Direction to the nearest drug store</h3>
+            <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nihil animi nam facere eveniet ipsum doloribus a itaque, delectus maxime quidem corporis rem magni? Vero adipisci quae laboriosam asperiores facilis!</p>
+          </span>
+        </div>
+        <div>
+          <img class="ello ml-20 hidden lg:flex" src="../assets/img/mock2.png" alt="">
+        </div>
+      </div>
+    </section>
+    <!-- eslint-disable-next-line max-len -->
+    <section class="flex flex-col lg:flex-row flex-no-wrap p-5 lg:px-24 py-8 justify-between w-full bg-blue-100">
+      <p class="text-3xl lg:text-5xl">Download on your device</p>
+      <a class="flex w-9/12 lg:w-3/12" href="">
+        <img class="object-contain" src="../assets/img/play.png" alt="">
+      </a>
+    </section>
+        <!-- eslint-disable-next-line max-len -->
     <section class="flex flex-col lg:flex-row flex-no-wrap lg:px-24 py-3 items-center justify-between w-full">
       <img class="flex w-full object-contain lg:w-7/12" src="../assets/img/talk2.png" alt="">
       <div class="flex flex-col">
@@ -65,17 +104,11 @@
         </div>
       </div>
     </section>
-    <!-- eslint-disable-next-line max-len -->
-    <section class="flex flex-col lg:flex-row flex-no-wrap p-5 lg:px-24 py-8 justify-between w-full bg-blue-100">
-      <p class="text-3xl lg:text-5xl">Download on your device</p>
-      <a class="flex w-9/12 lg:w-3/12" href="">
-        <img class="object-contain" src="../assets/img/play.png" alt="">
-      </a>
-    </section>
     <footer class="flex flex-row flex-no-wrap px-24 py-8 justify-between w-full">
-      <div class="font-semibold">
+      <div class="flex flex-col flex-no-wrap font-semibold">
         <p> &copy; copyright. Itoju 2020</p>
-        <a href="">Trep Labs Technologies</a>
+        <a class="hover:text-blue-600 hover:underline" href="">Trep Labs Technologies</a>
+        <a class="hover:text-blue-600 hover:underline" href="">Delveloper</a>
       </div>
     </footer>
   </div>
